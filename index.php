1433039d75cc9296dfac168e5db5d8390be78333
hippomu-media <74494691+hippomu-media@users.noreply.github.com>
2021-07-17 05:26:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,113 @@
-<?php 
-include('header.php');
-include_once("db_connect.php");
-?>
-<link rel="stylesheet" href="css/style.css" />
-<title>Demo of YouTube video downloader script in PHP</title>
- <meta name="keywords" content="Video downloader, download youtube, video download, youtube video, youtube downloader, download youtube FLV, download youtube MP4, download youtube 3GP, php video downloader" />
-<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Bootstrap Example</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
 <div class="container">
-	<h2>Demo - YouTube video downloader script in PHP</h2>	
-	<form class="form-download" method="get" id="download" action="getvideo.php">
-		<input type="text" name="videoid" style="height:30px" id="videoid" size="40" placeholder="YouTube Video ID" />
-		<input class="btn btn-primary" style="background:#3399ff;height:30px;color:white" type="submit" name="type" id="type"value="Download" />
-		<p>Put in just the ID, the part after v=.</p>
-		<p>Example: http://www.youtube.com/watch?v=<b>Wp-Dm-Swfeg</b></p>
-		<?PHP
-		include_once('config.php');
-		function is_chrome(){
-			$agent=$_SERVER['HTTP_USER_AGENT'];
-			if( preg_match("/like\sGecko\)\sChrome\//", $agent) ){	// if user agent is google chrome
-				if(!strstr($agent, 'Iron')) // but not Iron
-					return true;
-			}
-			return false;	// if isn't chrome return false
-		}
-		?>
-	</form>	
-	
+<form method="post" action="" class="formSmall">
+<div class="row">
+<div class="col-lg-12">
+<h7 class="text-align"> Download YouTube Video</h7>
 </div>
+<div class="col-lg-12">
+<div class="input-group">
+<input type="text" class="form-control" name="video_link" placeholder="Paste link.. e.g. https://www.youtube.com/watch?v=OK_JCtrrv-c">
+<span class="input-group-btn">
+<button type="submit" name="submit" id="submit" class="btn btn-primary">Go!</button>
+</span>
+</div><!-- /input-group -->
+</div>
+</div><!-- .row -->
+</form>
+<?php
+require_once "class.youtube.php";
+$yt  = new YouTubeDownloader();
+$downloadLinks ='';
+$error='';
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+$videoLink = $_POST['video_link'];
+if(!empty($videoLink)) {
+$vid = $yt->getYouTubeCode($videoLink);
+if($vid) {
+$result = $yt->processVideo($vid);
+if($result) {
+//print_r($result);
+$info = $result['videos']['info'];
+$formats = $result['videos']['formats'];
+$adapativeFormats = $result['videos']['adapativeFormats'];
+$videoInfo = json_decode($info['player_response']);
+$title = $videoInfo->videoDetails->title;
+$thumbnail = $videoInfo->videoDetails->thumbnail->thumbnails{0}->url;
+}
+else {
+$error = "Something went wrong";
+}
+}
+}
+else {
+$error = "Please enter a YouTube video URL";
+}
+}
+?>
+<?php if($formats):?>
+<div class="row formSmall">
+<div class="col-lg-3">
+<img src="<?php print $thumbnail?>">
+</div>
+<div class="col-lg-9">
+<?php print $title?>
+</div>
+</div>
+<div class="card formSmall">
+<div class="card-header">
+<strong>With Video & Sound</strong>
+</div>
+<div class="card-body">
+<table class="table ">
+<tr>
+<td>Type</td>
+<td>Quality</td>
+<td>Download</td>
+</tr>
+<?php foreach ($formats as $video) :?>
+<tr>
+<td><?php print $video['type']?></td>
+<td><?php print $video['quality']?></td>
+<td><a href="downloader.php?link=<?php print urlencode($video['link'])?>&title=<?php print urlencode($title)?>&type=<?php print urlencode($video['type'])?>">Download</a> </td>
+</tr>
+<?php endforeach;?>
+</table>
+</div>
+</div>
+<div class="card formSmall">
+<div class="card-header">
+<strong>Videos video only/ Audios audio only</strong>
+</div>
+<div class="card-body">
+<table class="table ">
+<tr>
+<td>Type</td>
+<td>Quality</td>
+<td>Download</td>
+</tr>
+<?php foreach ($adapativeFormats as $video) :?>
+<tr>
+<td><?php print $video['type']?></td>
+<td><?php print $video['quality']?></td>
+<td><a href="downloader.php?link=<?php print urlencode($video['link'])?>&title=<?php print urlencode($title)?>&type=<?php print urlencode($video['type'])?>">Download</a> </td>
+</tr>
+<?php endforeach;?>
+</table>
+</div>
+</div>
+<?php endif;?>
+</div>
+</body>
+</html>
